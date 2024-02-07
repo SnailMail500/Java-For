86c1eaf6d20c45fe51192add66b87e850d3d4d0f
Alex <83418697+SnailMail500@users.noreply.github.com>
2024-02-07 09:11:14 +0000

    Added notes on For Each Loops

--- Main.java
+++ Main.java
@@ -3,5 +3,11 @@ public class Main {
         for (int i = 0; i < 5; i++){//i as integer, i is less than 5, i increment 1
             System.out.println(i);//this will print i from 0 to 4
         }
+
+        //for each:
+        String[] cars = {"Audi", "Volvo", "VW", "Mercedes"};//array (scary!)
+        for (String i : cars){//for each item in the array, 
+            System.out.println(i);//print array[item]
+        }
     }
 }
