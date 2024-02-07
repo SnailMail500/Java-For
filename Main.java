9305269c5af75c6260f0182dbfa8a126b1c2e5fe
Alex <83418697+SnailMail500@users.noreply.github.com>
2024-02-07 09:01:12 +0000

    Added example of a simple for loop

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main (String[] args){
+        for (int i = 0; i < 5; i++){//i as integer, i is less than 5, i increment 1
+            System.out.println(i);//this will print i from 0 to 4
+        }
+    }
+}
